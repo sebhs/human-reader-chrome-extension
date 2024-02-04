f9f9836bddc1362175edaf2910926b2e2d4e095d
sebhs <s.hapteselassie@gmail.com>
2024-02-04 23:04:13 +0100

    refactor code to use manifest v3 and clean up the code

--- popup.js
+++ popup.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const apiKey = localStorage.getItem("apiKey");
-  const voiceId = localStorage.getItem("voiceId");
-  const mode = localStorage.getItem("mode")
-    ? localStorage.getItem("mode")
-    : "englishfast";
+  chrome.storage.local.get(["apiKey", "voiceId", "mode"], (result) => {
+    const apiKey = result.apiKey ? result.apiKey : "";
+    document.getElementById("apiKey").value = apiKey;
 
-  if (apiKey) document.getElementById("apiKey").value = apiKey;
-  if (voiceId) document.getElementById("voiceId").value = voiceId;
+    const voiceId = result.voiceId ? result.voiceId : "21m00Tcm4TlvDq8ikWAM";
+    document.getElementById("voiceId").value = voiceId;
+    chrome.storage.local.set({ voiceId: voiceId });
 
-  document.getElementById("mode").value = mode;
+    const mode = result.mode ? result.mode : "englishfast";
+    document.getElementById("mode").value = mode;
+    chrome.storage.local.set({ mode: mode });
+  });
 });
 
 function handleSetValue(inputId, buttonId, storageId) {
   const inputValue = document.getElementById(inputId).value;
   const button = document.getElementById(buttonId);
-  button.innerHTML = "&#10003;"; //checkmark
-  localStorage.setItem(storageId, inputValue);
-  setTimeout(function () {
-    button.textContent = "Set";
-  }, 1000);
+  chrome.storage.local.set({ [storageId]: inputValue }, () => {
+    button.innerHTML = "&#10003;"; //checkmark
+    setTimeout(function () {
+      button.textContent = "Set";
+    }, 1000);
+  });
 }
 
 document
@@ -32,13 +35,7 @@ document
     handleSetValue("voiceId", "setVoiceId", "voiceId")
   );
 
-document.addEventListener("DOMContentLoaded", () => {
-  const mode = localStorage.getItem("mode")
-    ? localStorage.getItem("mode")
-    : "englishfast";
-  document.getElementById("mode").value = mode;
-});
 document.getElementById("mode").addEventListener("change", () => {
   const mode = document.getElementById("mode").value;
-  localStorage.setItem("mode", mode);
+  chrome.storage.local.set({ mode: mode });
 });
